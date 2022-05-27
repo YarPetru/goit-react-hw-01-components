3ab96d1db3ae84a3379e675fee72e762bfb898f3
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-05-27 16:48:54 +0300

    first MVP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import Profile from './Profile/Profile';
+import u from './Profile/user.json';
+
+import StatList from './Statistics/StatList';
+import data from './Statistics/data.json';
+
+import FriendList from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
+
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <Profile
+        username={u.username}
+        tag={u.tag}
+        location={u.location}
+        avatar={u.avatar}
+        stats={u.stats}
+        // followers={u.stats.followers}
+        // views={u.stats.views}
+        // likes={u.stats.likes}
+      />
+      <StatList data={data} title="Test Title" />
+      <FriendList friendsData={friends} />
+      <TransactionHistory transactionsList={transactions} />
     </div>
   );
 };
